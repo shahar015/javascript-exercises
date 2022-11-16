b112a8172cda5ccf375b70fa82a89e6e49e9fa97
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-16 22:34:41 +0200

    Finish ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str='';
+    if (num<0)
+    str='ERROR';
+    for (let i=0; i<num; i++)
+    {
+        str+=string;
+    }
+    return str;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
