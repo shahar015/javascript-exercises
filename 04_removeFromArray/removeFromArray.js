c6de75eb0952172435d450f85cf708c14da62a32
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-16 23:22:02 +0200

    Finish ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
 const removeFromArray = function() {
-
-};
-
+    let arg=Array.from(arguments);
+    console.log(arg.join(","));
+    let arr=arg.shift();
+    console.log(arg.join(","));
+    let index;
+    for (let i=0; i<arg.length ; i++)
+    {
+    index=-1;
+    for (let a=0; a<arr.length; a++)
+    {
+        if(arr[a]===arg[i])
+        index=a;
+    }
+    if (index!=-1)
+    arr.splice(index,1);
+}
+    return arr;
+}
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
