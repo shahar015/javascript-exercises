45792d66bc1f8c2e5ccb8e830bf43d46d3e5808c
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-16 22:42:59 +0200

    Finish ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    strLst=str.split("");
+    strLst=strLst.reverse();
+    str=strLst.join("");
+    return str;
 };
-
+console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
