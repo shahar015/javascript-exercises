35c4bf769cc4d6e4b3ff18227a05fc23fabe3edb
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-17 19:49:20 +0200

    Finish ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  if (parseInt(num1) === num1 && parseInt(num2) === num2) {
+    return num1 + num2;
+  }
+  return "ERROR";
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  if (parseInt(num1) === num1 && parseInt(num2) === num2) {
+    return num1 - num2;
+  }
+  return "ERROR";
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let sum = 0;
+  let error = 0;
+  if (nums.length == 0) return sum;
+  for (let i = 0; i < nums.length; i++) {
+    if (parseInt(nums[i]) === nums[i]) sum += nums[i];
+    else error++;
+  }
+  if (error!=0) return "ERROR";
+  return sum;
+
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  let sum = 1;
+  let error = 0;
+  if (nums.length == 0) return sum;
+  for (let i = 0; i < nums.length; i++) {
+    if (parseInt(nums[i]) === nums[i]) sum *= nums[i];
+    else error++;
+  }
+  if (error!=0) return "ERROR";
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  if (parseInt(num1) === num1 && parseInt(num2) === num2) {
+    return num1 ** num2;
+  }
+  return "ERROR";
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let sum = 1;
+  if (parseInt(num) === num) {
+    for (let i = 1; i <= num; i++) {
+      sum *= i;;
+    }
+    return sum;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
@@ -29,5 +62,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
