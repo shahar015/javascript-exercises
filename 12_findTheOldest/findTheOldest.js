e2894d291733534471a29bc3d9aaba56c9b158b2
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-17 20:39:45 +0200

    Finish ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let max=-1;
+    let person;
+    let age;
+    for (let i=0; i<people.length; i++){
+        if(people[i].yearOfDeath!=undefined)
+        age=people[i].yearOfDeath-people[i].yearOfBirth;
+        else
+        age=2022-people[i].yearOfBirth;
+        max= Math.max(max,age);
+        if(max==age) person= people[i];
+    }
+    return person;
 };
 
 // Do not edit below this line
