08dc101ac179066c6491aa229c1fe62e97dfd1f6
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-17 20:00:32 +0200

    Finish ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strLst = [];
+  let final = [];
+  strLst = str.split("");
+  let index = 0;
+  for (let i = 0; i < strLst.length; i++) {
+    if (strLst[i].toLowerCase() != strLst[i].toUpperCase())
+      final[index] = strLst[i].toLowerCase();
+    index++;
+  }
+  if (final.join("") == final.reverse().join("") ) return true;
+  return false;
 };
-
+palindromes('tacos') ;
 // Do not edit below this line
 module.exports = palindromes;
