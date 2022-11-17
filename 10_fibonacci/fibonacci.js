fd961ff1f7956edd5217a237927a5e146b63db7c
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-17 20:20:39 +0200

    Finish ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let sum;
+  let lastNum =0;
+  let curNum=1;
+  if (Number(num) > 0) {
+    if(Number(num) == 1) return 1;
+    for (let i = 1; i < Number(num); i++) {
+        sum = lastNum + curNum;
+        lastNum = curNum;
+        curNum = sum;
+    }
+    return sum;
+  }
+  return "OOPS";
 };
 
 // Do not edit below this line
