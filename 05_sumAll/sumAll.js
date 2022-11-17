d7f82944efce54c39a22679884b0e63c79c41531
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-17 18:54:50 +0200

    Finish ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0 || num1 !== parseInt(num1)|| num2 !== parseInt(num2)) {
+    return "ERROR";
+  }
+  let sum = 0;
+  for (let i = Math.min(num1, num2); i <= Math.max(num2, num1); i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
