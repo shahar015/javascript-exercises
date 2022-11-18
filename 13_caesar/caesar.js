e37cc9c366d37f4ac9e9dba36c5e6c1b80dfca22
Shahar Zarfati <Shahar0151@gmail.com>
2022-11-18 15:49:58 +0200

    Finish last exercises
    finished 13th ex and with that finished al exercises

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,59 @@
-const caesar = function() {
-
+const caesar = function(str, num) {
+    strLst=str.split("");
+    for(let i=0; i<str.length; i++)
+    {
+        strLst[i]=encode(strLst[i],num);
+    }
+    return strLst.join("");
 };
+function encode(char,num){
+    let uni=Number(char.charCodeAt(0));
+    let newUni;
+        if(uni>=65&& uni<=90)
+        {
+            if(uni+num>90)
+            {
+                newUni=uni+num-(Math.ceil(num/26)*26)
+            }
+            else if(uni+num<65)
+            {
+                if(Math.floor(-num/26)!=0)
+                newUni=uni+num+(Math.floor(-num/26)*26)
+                else
+                newUni=uni+num+26;
+            }
+            else
+            {
+                newUni=uni+num;
+            }
+        }
+        
+        else if(uni>=97&& uni<=122)
+        {
+            if(uni+num>122)
+            {
+                newUni=uni+num-(Math.ceil(num/26)*26)
+            }
+            else if(uni+num<97)
+            {
+                if(Math.floor(-num/26)!=0)
+                newUni=uni+num+(Math.floor(-num/26)*26)
+                else
+                newUni=uni+num+26;
+            }
+            else
+            {
+                newUni=uni+num;
+            }
+        }
+        else
+        {
+            newUni=uni;
+        }
+        return String.fromCharCode(newUni);
+
+}
+console.log(caesar('a',-1));
 
 // Do not edit below this line
 module.exports = caesar;
